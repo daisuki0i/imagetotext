31929bc19a66ce2ce43fbfff9d0b94f6b34780ae
M <xboc12349asd@gmail.com>
2024-09-15 18:45:20 +0700

    Box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
     // นำเข้า Hometext.svelte เพื่อใช้งานในหน้า +page.svelte
     import Hometext from './Hometext.svelte'; 
+    import Box from './Box.svelte';    
 </script>
 
 <!-- เรียกใช้คอมโพเนนต์ Hometext -->
 <Hometext />
-
-
+<Box />
